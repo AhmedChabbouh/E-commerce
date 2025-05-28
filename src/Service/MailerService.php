9c41ee27682bc3579a490a531a0e6e383f039a5a
ghassen naouar <ghassennaouar7@gmail.com>
2025-05-28 10:49:26 +0100

    Mailer Service code(still working on)

---
+++ src/Service/MailerService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+class MailerService
+{
+    public function __construct(private MailerInterface $mailer){}
+    public function sendEmail($to = 'ghassennaouar7@example.com'): void
+    {
+        $email = (new Email())
+            ->from('symfonyecommerce34@gmail.com')
+            ->to($to)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>helliw !!</p>');
+
+        $this->mailer->send($email);
+
+    }
+
+}
