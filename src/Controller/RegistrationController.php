0b27f48c673a2ee5802ab28cfc7c1da99e28f8f4
ghassen naouar <ghassennaouar7@gmail.com>
2025-05-29 19:58:56 +0100

    added mail confirmation twig + added our e-commerce google account

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('website@noreply.com', 'E-Commerce No-Reply'))
+                    ->from(new Address('symfonyecommerce34@gmail.com', 'E-Commerce No-Reply'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
