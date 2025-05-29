2163607dd3c5ea28079ace9b50cef023630f98c1
dorra dorra <dorra.ourabi@insat.ucar.tn>
2025-05-29 19:29:28 +0100

    added the home controller / home page/designed the pages

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -24,12 +24,25 @@ final class CategoryController extends AbstractController
         $category3->setName("Oiseau");
         $category4=new category();
         $category4->setName("poisson");
+        $category5=new category();
+        $category5->setName("Aliments");
+        $category6=new category();
+        $category6->setName("Habitats");
+        $category7=new category();
+        $category7->setName("3alloush el 3id");
 
-       $entityManager->persist($category);
-       $entityManager->persist($category1);
-       $entityManager->persist($category2);
-       $entityManager->persist($category3);
-       $entityManager->persist($category4);
+
+
+       // $entityManager->persist($category);
+
+      // $entityManager->persist($category1);
+     //  $entityManager->persist($category2);
+      // $entityManager->persist($category3);
+      // $entityManager->persist($category4);
+
+      $entityManager->persist($category5);
+      $entityManager->persist($category6);
+      $entityManager->persist($category7);
        $entityManager->flush();
        return new Response("category created");
     }
